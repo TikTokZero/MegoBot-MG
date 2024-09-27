2a8c1fcf3438f4f5a740bcab267b93823a4732b3
TikTokZero <zhranmhmwdmhmd085@gmail.com>
2024-09-27 12:05:22 +0300

    Update اعاده.js

--- plugins/اعاده.js
+++ plugins/اعاده.js
@@ -15,9 +15,9 @@ let handler = async (m, { conn, text }) => {
 
   if(!text && !m.quoted) return conn.reply(m.chat, `*❏إعادة تعيين المستخدم*\n\nمنشن المستخدم, *اكتب الرقم أو قم بالرد على رسالة المستخدم الذي تريد *إعادة تعيينه`, m)
   //let exists = await conn.isOnWhatsApp(number)
-  // if (exists) return conn.reply(m.chat, `*الرقم غير مسجل في واتس اب*`, m)
+  // if (exists) return conn.reply(m.chat, `*الرقم مش متسجل ف الواتس اثلا يحتة 😐*`, m)
   if(isNaN(number)) return conn.reply(m.chat, `*❏ إعادة تعيين المستخدم*\nالرقم الذي ادخلته خاطأً`, m)
- // if(number.length > 8) return conn.reply(m.chat, `*❏ إعادة تعيين المستخدم*\nالرقم الذي ادخلته خاطأً!`, m)
+ // if(number.length > 8) return conn.reply(m.chat, `*❏ إعادة تعيين المستخدم*\nالرقم اللي انت حطيتة غلط🥲!`, m)
   try {
     if(text) {
       var user = number + '@s.whatsapp.net'
