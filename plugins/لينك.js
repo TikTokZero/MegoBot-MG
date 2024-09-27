3f762c6fdbde4d70ccd6dafe6a7b8a0d11c02d77
𖤍𝑷𝑨𝑷𝑳𝑶 𝑯𝑨𝑪𝑲𝑰𝑵𝑮𖤍 <zhranmhmwdmhmd085@gmail.com>
2024-09-27 14:16:25 +0300

    Update لينك.js

--- plugins/لينك.js
+++ plugins/لينك.js
@@ -4,9 +4,9 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
       title: 'لينك الجروب',
-      body: 'VENOM 𝐵𝛩𝑇',
+      body: '𝑷𝑨𝑷𝑳𝑶 𝐵𝛩𝑇',
       previewType: 0, thumbnail: fs.readFileSync('./Menu.jpg'),
-      sourceUrl: `https://chat.whatsapp.com/Bu7cwDjLYwLJ93yyUD1tE1`}}});
+      sourceUrl: `https://chat.whatsapp.com/DsuuBwr5tVTFmujOWfEBhn`}}});
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
