f97c3f07b3395cc6df5b36edc3a6a73fe4c1f194
𖤍𝑷𝑨𝑷𝑳𝑶 𝑯𝑨𝑪𝑲𝑰𝑵𝑮𖤍 <zhranmhmwdmhmd085@gmail.com>
2024-09-27 12:26:47 +0300

    Update بان.js

--- plugins/بان.js
+++ plugins/بان.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!who) throw `□ منشن الشخص`
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `@${who.split`@`[0]} لن تستطيع استخدام اوامر بعد الان !`, m, { mentions: [who] })
+    conn.reply(m.chat, `@${who.split`@`[0]} مش هتعرف تستخدم الاوامر تاني المطور 𝑷𝑨𝑷𝑳𝑶 𝑯𝑨𝑪𝑲𝑰𝑵𝑮 بندك!`, m, { mentions: [who] })
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
