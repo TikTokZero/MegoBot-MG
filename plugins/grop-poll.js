830261678e353d8f8f16fb66ff29e79312e9fbd3
TikTokZero <zhranmhmwdmhmd085@gmail.com>
2024-09-27 11:33:05 +0300

    Update grop-poll.js

--- plugins/grop-poll.js
+++ plugins/grop-poll.js
@@ -1,13 +1,13 @@
-//ميجو يحبكم 🤭❤️
+//بابلو يحبكم 🤭❤️
 //قناه المصدر
-//https://whatsapp.com/channel/0029VaRygQcATRSk29RI4P1x
+//https://whatsapp.com/channel/0029VapvokVCHDyj6inRiP3T
 
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   let name = await conn.getName(m.sender);
   if (name == 'undefined') name = 'Indefinido';
   const b = text.split('|');
-  if (!b[1]) throw `*⚠️ الامر بيشتغل كدا ${usedPrefix + command} الافضل؟|ايتاتشي|مادرا*`;
-  if (b[12]) throw `*⚠️ الامر بيشتغل كدا  ${usedPrefix + command} الافضل؟|ايتاتشي|مادرا*`;
+  if (!b[1]) throw `*⚠️ الامر بيشتغل كدا ${usedPrefix + command}الافضل؟|بابلو|سكار*`;
+  if (b[12]) throw `*⚠️ الامر بيشتغل كدا  ${usedPrefix + command} الافضل؟|بابلو|سكار*`;
   const caption = `*مقدم التصويت:*\n${name}\n*السؤال ↧↧:*\n${text.split('|')[0]}`.trim();
   const options = text.split("|").slice(1).map(option => ({ optionName: option.trim() }));  
   const sendPollMessage = {
